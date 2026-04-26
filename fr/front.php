40ad76e051a7e4c0ee3c11ce7c205d9e4d074b70
ewigbaum <yancyjane27@gmail.com>
2026-04-26 23:45:47 +0200

    language.php + front.php somewhat done

---
+++ fr/front.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'current' => 'Actuellement au front',
+    'none' => 'Personne en front en ce moment.',
+    'started' => 'Front a commencé.',
+    'history' => 'Historique de fronts',
+    'comment' => 'Commentaire',
+    'co_fronting' => 'Co-front',
+    'co_con' => 'Co-con',
+    'duration' => 'Duration',
+    'started_at' => 'Commencé à',
+    'ended_at' => 'Terminé à',
+    'started_fronting' => 'est en front.',
+    'stopped_fronting' => 'a quitté le front.',
+    'changed_front_type' => 'changé de :old à :new.',
+];
