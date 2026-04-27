0bcbaa8e1a6ef86df0a1aea365a238e80bd12bf2
ewigbaum <yancyjane27@gmail.com>
2026-04-27 21:24:57 +0200

    small edits

--- fr/auth.php
+++ fr/auth.php
@@ -3,5 +3,5 @@
 return [
     'failed' => 'Ces identifiants correspondent pas à nos rapports.',
     'password' => 'Le mot de passe saisi est incorrect.',
-    'throttle' => 'Trop de tentatives de connextion; Merci de réessayer dans :seconds secondes.',
+    'throttle' => 'Trop de tentatives de connection. Merci de réessayer dans :seconds secondes.',
 ];
