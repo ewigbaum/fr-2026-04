30c9d0012d984cb87767d47c1d58482a5a7add7a
ewigbaum <yancyjane27@gmail.com>
2026-04-27 14:09:10 +0200

    some edits

--- fr/passwords.php
+++ fr/passwords.php
@@ -2,8 +2,8 @@
 
 return [
     'reset' => 'Ton mot de passe a été réinitialisé.',
-    'sent' => 'Si cet mail est dans notre système, nous avons envoyé un lien de réinitialisation. Vérifier ta boîte de message.',
+    'sent' => 'Si ce mail est bien dans notre système, nous t\'avons envoyé un lien de réinitialisation. Vérifies ta boîte mail.',
     'throttled' => 'Merci d\'attendre avant de réessayer.',
     'token' => 'Ce jeton de réinitialisation de mot de passe n\'est pas valide.',
-    'user' => 'Nous avons pas pu trouvé un utilisateur avec cet mail.',
+    'user' => 'Nous n\'avons pas pu trouvé d\'utilisateur avec cette adresse  mail.',
 ];
